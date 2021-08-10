7bf47143d6a7ab5833f37cf339b3df0861a4723c
Sushmita Gupta <sush18nov95@gmail.com>
2021-08-10 16:11:45 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <Router>
+    <GlobalStyle />  
+      <Hero />
+    </Router>
+  );
+}
+
+export default App;
